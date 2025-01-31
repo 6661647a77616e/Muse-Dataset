445d86cfb9b6f905b5971d2404cc0221318d3df3
fadzwan <fadzwanashriq.m@gmail.com>
2025-01-31 13:33:08 +0800

    add other group dataset

--- eda.py
+++ eda.py
@@ -11,33 +11,38 @@ eyes_closed_paths = [
     "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-01/A1_01_EC.csv",
     "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-02/A1_02_EC.csv",
     "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-03/A1_03_EC.csv",
-    # "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-04/A1_04_EC.csv",
+    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-04/A1_04_EC.csv",
     "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-05/A1_05_EC.csv",
     "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-06/A1_06_EC.csv",
     "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-07/A1_07_EC.csv"
 ]
 
 eyes_open_paths = [
-    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-01/A1_01_EO.csv",
-    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-02/A1_02_EO.csv",
-    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-03/A1_03_EO.csv",
-    # "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-04/A1_04_EO.csv",
-    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-05/A1_05_EO.csv",
-    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-06/A1_06_EO.csv",
-    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/A1-07/A1_07_EO.csv"
+    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/dataset/A1-01/A1_01_EO.csv",
+    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/dataset/A1-02/A1_02_EO.csv",
+    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/dataset/A1-03/A1_03_EO.csv",
+    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/dataset/A1-04/A1_04_EO.csv",
+    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/dataset/A1-05/A1_05_EO.csv",
+    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/dataset/A1-06/A1_06_EO.csv",
+    "https://raw.githubusercontent.com/6661647a77616e/Muse-Dataset/main/dataset/A1-07/A1_07_EO.csv"
 ]
-
-# Define the metadata manually
 data = [
     (17, 24, 16, 20.67, 32, "A1-01", "Imran"),
     (12, 32, 19, 13.00, 45, "A1-02", "Ijat"),
     (22, 27, 21, 3.67, 30, "A1-03", "Malik"),
-    # (18, 27, 14, 15.67, 29, "A1-04", "Faris"),
+    (18, 27, 14, 15.67, 29, "A1-04", "Faris"),
     (25, 16, 21, 16.00, 36, "A1-05", "Anif"),
     (14, 22, 16, 11.33, 24, "A1-06", "Taib"),
-    (17, 27, 18, 9.00, 29, "A1-07", "Syazwan")
+    (17, 27, 18, 9.00, 29, "A1-07", "Syazwan"),
+    (17, 26, 19, 34, "C-01", "Dahlia Binti Husin"),
+    (13, 21, 28, 26, 38, "C-02", "Nur Fathnin Ilhami Bt Mohamad Helmi"),
+    (20, 13, 13, 21.67, 27, "C-03", "Noor Azzatul Sofea Binti Yayah"),
+    (20, 29, 22, 13, 31, "C-04", "Raihanatuzzahra Binti Azmi"),
+    (20, 26, 9, 14, 33, "C-05", "Hayani Nazurah Binti Hasram"),
+    (19, 18, 7, 23, 39, "C-06", "Nur Syakirah Huda Binti Razali"),
+    (10, 26, 19, 17.67, 27, "C-07", "Qhuratul Nissa Binti Mior Idris"),
+    (24, 17, 15, 23.33, 34, "C-08", "Ainul Mardhiah Binti Mohammed Rafiq")
 ]
-
 # Create a dictionary mapping each subject ID to their corresponding details
 dataset_dict = {}
 
